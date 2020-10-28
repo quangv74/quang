8a14aee4416051ea2475a003432abad0ef8b3598
quangv74 <72604737+quangv74@users.noreply.github.com>
2020-10-28 16:52:19 +0700

    Add files via upload

---
+++ Code.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratice;
+
+/**
+ *
+ * @author my laptop
+ */
+public class Code {
+    private String code ; 
+    private String name ; 
+    private int credit ;
+
+    public Code() {
+    }
+
+    public Code(String code, String name, int credit) {
+        this.code = code;
+        this.name = name;
+        this.credit = credit;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return code + "\t" + name + "\t" + credit ;
+    }
+    
+}
